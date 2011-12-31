4c7ca2cb656f7e544cc70640567d34d4ca681e33
Inspiran <daniel@inspiran.be>
2012-01-01 00:52:45 +0100

    Added vendors.php

---
+++ vendor/vendors.php
@@ -0,0 +1,35 @@
+#!/usr/bin/env php
+<?php
+
+/*
+ * This file is part of the VespolinaCartBundle package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+set_time_limit(0);
+
+$vendorDir = __DIR__;
+$deps = array(
+    array('symfony', 'http://github.com/symfony/symfony', isset($_SERVER['SYMFONY_VERSION']) ? $_SERVER['SYMFONY_VERSION'] : 'origin/master'),
+    array('doctrine-common', 'http://github.com/doctrine/common.git', 'origin/master'),
+    array('doctrine-dbal', 'http://github.com/doctrine/dbal.git', 'origin/master'),
+    array('doctrine', 'http://github.com/doctrine/doctrine2.git', 'origin/master'),
+    array('doctrine-mongodb-odm', 'http://github.com/doctrine/mongodb-odm.git', 'origin/master'),
+    array('doctrine-mongodb', 'http://github.com/doctrine/mongodb.git', 'origin/master'),
+    array('doctrine-couchdb', 'http://github.com/doctrine/couchdb-odm.git', 'origin/master'),
+);
+
+foreach ($deps as $dep) {
+    list($name, $url, $rev) = $dep;
+
+    echo "> Installing/Updating $name\n";
+
+    $installDir = $vendorDir.'/'.$name;
+    if (!is_dir($installDir)) {
+        system(sprintf('git clone -q %s %s', escapeshellarg($url), escapeshellarg($installDir)));
+    }
+
+    system(sprintf('cd %s && git fetch -q origin && git reset --hard %s', escapeshellarg($installDir), escapeshellarg($rev)));
+}
