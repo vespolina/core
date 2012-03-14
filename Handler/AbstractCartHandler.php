7d3d74edb96b913098168c6575059c243e32a93a
IamPersistent <develop@zestic.com>
2012-03-14 01:30:39 -0700

    add Handler interface and base class

---
+++ Handler/AbstractCartHandler.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * (c) 2012 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Vespolina\CartBundle\Handler;
+
+use Vespolina\CartBundle\Handler\CartHandlerInterface;
+use Vespolina\CartBundle\Model\CartItemInterface;
+
+/**
+ * This provides a default set of actions for the methods that can be used by any other CartHandler by extending this class
+ */
+abstract class AbstractCartHandler implements CartHandlerInterface
+{
+    public function determineCartItemPrices(CartItemInterface $cartItem, $pricingContext)
+    {
+        throw new \Exception("Sorry determineCartItemPrices() doesn't have default functionality in place");
+    }
+}
