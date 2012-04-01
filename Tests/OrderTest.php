2f50d01c8c22190dbb74244a55db8ada850dae73
IamPersistent <develop@zestic.com>
2012-04-01 02:24:08 -0700

    add empty test class for Order

---
+++ Tests/OrderTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * (c) 2012 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Vespolina\Entity\Tests;
+
+use Vespolina\Entity\Order;
+
+class OrderTest extends \PHPUnit_Framework_TestCase
+{
+
+}
