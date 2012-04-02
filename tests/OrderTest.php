4d6929e5cd21ac719fa18c5e649e649fb25bda97
IamPersistent <develop@zestic.com>
2012-04-02 07:53:46 -0700

    add Item test for Order

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -9,8 +9,38 @@
 namespace Vespolina\Entity\Tests;
 
 use Vespolina\Entity\Order;
+use Vespolina\Entity\Item;
 
 class OrderTest extends \PHPUnit_Framework_TestCase
 {
+    public function testItems()
+    {
+        $order = new Order();
+        $this->assertNull($order->getItems(), 'make sure we start out empty');
 
+        $item = new Item();
+        $order->addItem($item);
+        $this->assertType('array', $order->getItems());
+        $this->assertContains($item, $order->getItems());
+        $this->assertSame(1, count($order->getItems()));
+
+        $items = array();
+        $items[] = new Item();
+        $items[] = new Item();
+        $order->mergeItems($items);
+        $this->assertSame(3, $order->getItems());
+
+        $order->removeItem($item);
+        $this->assertNotContains($item, $order->getItems());
+        $this->assertSame(2, $order->getItems());
+
+        $order->clearItems();
+        $this->assertEmpty($order->getItems());
+
+        $order->addItem($item);
+        $order->setItems($items);
+        $this->assertNotContains($item, $order->getItems(), 'this should have been removed on setting a new array of items');
+        $this->assertSame(2, $order->getItems());
+
+    }
 }
