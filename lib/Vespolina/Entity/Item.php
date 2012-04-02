b34c24ac4feb76c162003395603c28a2ecf875f7
IamPersistent <develop@zestic.com>
2012-04-02 07:58:20 -0700

    add Item and ItemInterface

---
+++ lib/Vespolina/Entity/Item.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * (c) 2012 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Vespolina\Entity;
+
+use Vespolina\Entity\ItemInterface;
+
+/**
+ * Item is a class for items in an order
+ *
+ * @author Richard Shank <develop@zestic.com>
+ */
+class Item implements ItemInterface
+{
+
+}
