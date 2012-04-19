34de632fa7bbfdb441f354cc78504784637aa1ce
IamPersistent <develop@zestic.com>
2012-04-18 22:23:12 -0700

    move identifiers from ProductBundle into entity

---
+++ lib/Vespolina/Entity/Exception/FunctionNotSupportedException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * (c) 2012 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace Vespolina\Entity\Exception;
+
+class FunctionNotSupportedException extends \Exception
+{
+
+}
---
+++ lib/Vespolina/Entity/Indentifier/UPCIdentifier.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * (c) 2011-2012 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace  Vespolina\Entity\Identifier;
+
+use Vespolina\Entity\GS1Identifier;
+
+/**
+ * @author Richard D Shank <develop@zestic.com>
+ */
+class UPCIdentifier extends GS1Identifier
+{
+    /**
+     * Performs a redundancy check on the identifier code
+     *
+     * @return boolean
+     */
+    public function checkDigit($code = null)
+    {
+        return false;
+    }
+
+    public function getName()
+    {
+        return 'upc';
+    }
+}
