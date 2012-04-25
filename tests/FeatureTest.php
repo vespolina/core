4a8ccbfe51675dd04ed47fefbff9986b87b856c1
IamPersistent <develop@zestic.com>
2012-04-25 10:49:55 -0700

    move feature test over from product bundle

--- tests/FeatureTest.php
+++ tests/FeatureTest.php
@@ -17,5 +17,32 @@ class FeatureTest extends \PHPUnit_Framework_TestCase
         $feature = new Feature();
         $feature->setSearchTerm('MIxEd cAsE');
         $this->assertSame('mixed case', $feature->getSearchTerm(), 'the search term should be converted to lower case before setting');
+
+        $titleFeature = new Feature();
+        $titleFeature->setType('title');
+        $titleFeature->setName('EIGHT53');
+
+        $this->assertEquals(
+            'eight53',
+            $titleFeature->getSearchTerm(),
+            'the search term should be a lowercase version of the name'
+        );
+
+        $titleFeature->setSearchTerm('different search term');
+        $this->assertEquals(
+            'different search term',
+            $titleFeature->getSearchTerm(),
+            'setting search term overrides previous set term'
+        );
+
+        $titleFeature->setName('eight53');
+        $this->assertEquals(
+            'different search term',
+            $titleFeature->getSearchTerm(),
+            'if a term is already set, it should not be overwritten by setting the name'
+        );
+
+        $titleFeature->setSearchTerm(21);
+        $this->assertInternalType('string', $titleFeature->getSearchTerm(), 'make sure the search is type cast as a string');
     }
 }
