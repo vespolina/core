647d5ff46f525423febd2590dae3834b5a7dbc2a
iampersistent <develop@zestic.com>
2012-07-15 19:06:11 -0700

    add some simple event dispatching

---
+++ lib/Vespolina/EventDispatcher/EventDispatcherInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * (c) 2012 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Vespolina\EventDispatcher;
+
+use Vespolina\EventDispatcher\EventInterface;
+
+interface EventDispatcherInterface
+{
+    function dispatch($eventName, EventInterface $event = null);
+}
