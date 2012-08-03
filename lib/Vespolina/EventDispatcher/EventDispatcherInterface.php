0a87311b20eaf3e310ff38356043e08dd30af398
iampersistent <develop@zestic.com>
2012-08-03 13:28:13 -0700

    remove name from the createEvent params, add api docs

--- lib/Vespolina/EventDispatcher/EventDispatcherInterface.php
+++ lib/Vespolina/EventDispatcher/EventDispatcherInterface.php
@@ -12,7 +12,24 @@ use Vespolina\EventDispatcher\EventInterface;
 
 interface EventDispatcherInterface
 {
-    function createEvent($name, $subject=null);
+    /**
+     * Create a new Event for the dispatcher system
+     *
+     * @param null $subject object that is the subject of the event
+     *
+     * @return instance Vespolina\EventDispatcher\EventInterface
+     */
+    function createEvent($subject=null);
 
+    /**
+     * Dispatches an event to all registered listeners.
+     *
+     * @param string $eventName The name of the event to dispatch. The name of the event is the name of the method that
+     *                          is invoked on listeners.
+     * @param Vespolina\EventDispatcher\EventInterface $event The event to pass to the event handlers/listeners.
+     *                          If not supplied, an empty Event instance is created.
+     *
+     * @return Event
+     */
     function dispatch($eventName, EventInterface $event = null);
 }
