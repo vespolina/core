55d41673960c9c72f5d37cbf2721eef5a0ffa163
iampersistent <develop@zestic.com>
2012-09-02 11:07:52 -0700

    extract a base product class to be shared between product and merchandise

---
+++ lib/Vespolina/Entity/Product/BaseProductInterface.php
@@ -0,0 +1,189 @@
+<?php
+/**
+ * (c) 2011-2012 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Vespolina\Entity\Product;
+
+use Vespolina\Entity\Product\FeatureInterface;
+use Vespolina\Entity\Identifier\IdentifierInterface;
+use Vespolina\Entity\Product\OptionInterface;
+use Vespolina\Entity\Product\OptionGroupInterface;
+
+/**
+ * @author Richard D Shank <develop@zestic.com>
+ * @author Daniel Kucharski <daniel@xerias.be>
+ */
+interface BaseProductInterface
+{
+    /**
+     * Set the description of the product
+     *
+     * @param $description
+     */
+    function setDescription($description);
+
+    /**
+     * Return the description of the product
+     *
+     * @return string
+     */
+    function getDescription();
+
+    /**
+     * Remove all features from the product
+     */
+    function clearFeatures();
+
+    /**
+     * Return the features of the product
+     *
+     * @return \Vespolina\Entity\Product\FeatureInterface
+     */
+    function getFeatures();
+
+    /**
+     * Remove a feature by its type
+     *
+     * @param $type
+     */
+    function removeFeature($type);
+
+    /**
+     * Set the features of the product to a feature set
+     *
+     * @param $features
+     */
+    function setFeatures($features);
+
+    /**
+     * Set the name of the product
+     *
+     * @param $name
+     */
+    function setName($name);
+
+    /**
+     * Return the name of the product
+     *
+     * @return string
+     */
+    function getName();
+
+    /**
+     * Add an option set to the product
+     *
+     * @param \Vespolina\Entity\Product\OptionGroupInterface $optionGroup
+     *
+     */
+/** remove for now to get feature tests to pass
+    function addOptionGroup(OptionGroupInterface $optionGroup);
+*/
+    /**
+     * Remove an options set from the product
+     *
+     * @param string name of group $name
+     */
+    function removeOptionGroup($name);
+
+    /**
+     * Set the options of the product to an option set
+     *
+     * @param array of Vespolina\ProductBundle\Node\OptionGroupInterface $optionGroup
+     */
+    function setOptions($options);
+
+    /**
+     * Remove the option groups from the project
+     *
+     */
+    function clearOptions();
+
+    /**
+     * Return the options of the product
+     *
+     * @return array of Vespolina\ProductBundle\Option\OptionGroupInterface
+     */
+    function getOptions();
+
+    /**
+     * Return the identifier set generated from the option choices
+     *
+     * @return array of Vespolina\ProductBundle\Identifier\ProductIdentifierSetInterface
+     */
+    function getIdentifierSets();
+
+    /**
+     * Return an identifier set for the option set combination
+     *
+     * @param array (optional) option set or null returns primary
+     *
+     * @return Vespolina\ProductBundle\Identifier\ProductIdentifierSetInterface
+     */
+    function getIdentifierSet($target = null);
+
+    /**
+     * Add an identifier to an identifier set. No target adds identifier to primary.
+     *
+     * @param $identifier
+     * @param $target
+     */
+    function addIdentifier($identifier, $target = null);
+
+    /**
+     * Use a different name or different technique
+     *
+     * These are valid types of products
+     * Product::PHYSICAL
+     * Product::UNIQUE
+     * Product::DOWNLOAD
+     * Product::TIME
+     * Product::SERVICE
+     *
+     * @param $type
+     */
+
+    /**
+     * Set the product type, ie shirt, cd, tickets
+     *
+     * @param $type
+     */
+    function setType($type);
+
+    /**
+     * Get the product type.
+     * @return type
+     */
+    function getType();
+
+    /**
+     * Return the cart creation date
+     *
+     * @return
+     */
+    function getCreatedAt();
+
+    /**
+     * Set the cart creation date
+     *
+     * @param DateTime $createdAt
+     */
+    function setCreatedAt(\DateTime $createdAt);
+
+    /**
+     * Return the cart update date
+     *
+     * @return
+     */
+    function getUpdatedAt();
+
+    /**
+     * Set the cart update date
+     *
+     * @param DateTime $updatedAt
+     */
+    function setUpdatedAt(\DateTime $updatedAt);
+}
