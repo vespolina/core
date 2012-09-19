6f72b8361174c43a51031033bec63bbaa10a34f1
iampersistent <develop@zestic.com>
2012-09-18 17:27:40 -0700

    add SalesChannel to core

---
+++ lib/Vespolina/Entity/SalesChannel/SalesZone.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * (c) 2012 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Vespolina\Entity\SalesChannel;
+
+use Vespolina\Entity\SalesChannel\SalesZoneInterface;
+
+class SalesZone implements SalesZoneInterface
+{
+
+}
