7a43444375ba710a08b4bfa5c2843576526f6a90
iampersistent <develop@zestic.com>
2013-01-12 00:34:16 -0800

    add methods to invoice class

--- lib/Vespolina/Entity/Invoice/InvoiceInterface.php
+++ lib/Vespolina/Entity/Invoice/InvoiceInterface.php
@@ -9,6 +9,7 @@
 namespace Vespolina\Entity\Invoice;
 
 use Vespolina\Entity\Order\OrderInterface;
+use Vespolina\Entity\Partner\PartnerInterface;
 
 /**
  * An interface for an invoice for an order
@@ -17,5 +18,124 @@ use Vespolina\Entity\Order\OrderInterface;
  */
 interface InvoiceInterface
 {
+    /**
+     * Set the due date of this invoice
+     *
+     * @param \DateTime $dueDate
+     * @return $this
+     */
+    function setDueDate(\DateTime $dueDate);
 
+    /**
+     * Return the due date of this invoice
+     *
+     * @return \DateTime $dueDate
+     */
+    function getDueDate();
+
+    /**
+     * Set the issued date of this invoice
+     *
+     * @param \DateTime $issuedDate
+     * @return $this
+     */
+    function setIssuedDate(\DateTime $issuedDate);
+
+    /**
+     * Return the issue date of this invoice
+     *
+     * @return \DateTime $dueDate
+     */
+    function getIssuedDate();
+
+    /**
+     * Add an order to be billed in this invoice
+     *
+     * @param \Vespolina\Entity\Order\OrderInterface $order
+     * @return $this
+     */
+    function addOrder(OrderInterface $order);
+
+    /**
+     * Remove all orders from this invoice
+     *
+     * @return $this
+     */
+    function clearOrders();
+
+    /**
+     * Return the orders with this invoice
+     *
+     * @return array
+     */
+    function getOrders();
+
+    /**
+     * Merge a group of orders with the orders in this invoice
+     *
+     * @param array $orders
+     * @return $this
+     */
+    function mergeOrders(array $orders);
+
+    /**
+     * Remove a specific order from the invoice
+     *
+     * @param \Vespolina\Entity\Order\OrderInterface $order
+     * @return $this
+     */
+    function removeOrder(OrderInterface $order);
+
+    /**
+     * Set a group of orders for this invoice
+     *
+     * @param array $orders
+     * @return $this
+     */
+    function setOrders(array $orders);
+
+    /**
+     * Set the Partner getting billed with this invoice
+     *
+     * @param \Vespolina\Entity\Partner\PartnerInterface $partner
+     * @return $this
+     */
+    function setPartner(PartnerInterface $partner);
+
+    /**
+     * Return the partner getting billed in this invoice
+     *
+     * @return \Vespolina\Entity\Partner\PartnerInterface
+     */
+    function getPartner();
+
+    /**
+     * Set the payment information from the billing
+     *
+     * @param $payment
+     * @return $this
+     */
+    function setPayment($payment);
+
+    /**
+     * Return the payment information for this billing
+     *
+     * @return
+     */
+    function getPayment();
+
+    /**
+     * Set previously created Invoice for the Partner, can be used for reporting
+     *
+     * @param \Vespolina\Entity\Partner\InvoiceInterface $previousInvoice
+     * @return $this
+     */
+    function setPreviousInvoice(InvoiceInterface $previousInvoice);
+
+    /**
+     * Return the previous invoice for the Partner
+     *
+     * @return \Vespolina\Entity\Partner\InvoiceInterface
+     */
+    function getPreviousInvoice();
 }
