825830fb83aab22ced707a024b7d62271c7da63f
iampersistent <develop@zestic.com>
2013-01-11 18:41:04 -0800

    move invoice classes into Invoice namespace

--- lib/Vespolina/Entity/Invoice/InvoiceInterface.php
+++ lib/Vespolina/Entity/Invoice/InvoiceInterface.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * (c) 2012 Vespolina Project http://www.vespolina-project.org
+ * (c) 2012-2013 Vespolina Project http://www.vespolina-project.org
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
 
-namespace Vespolina\Entity;
+namespace Vespolina\Entity\Invoice;
 
-use Vespolina\Entity\OrderInterface;
+use Vespolina\Entity\Order\OrderInterface;
 
 /**
  * An interface for an invoice for an order
