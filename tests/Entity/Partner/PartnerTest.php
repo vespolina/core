9544c48b58c029ea376845d138e6f97e228bed76
Luis Cordova <cordoval@gmail.com>
2013-01-16 11:43:19 -0500

    make partner more fluent and fix test

--- tests/Entity/Partner/PartnerTest.php
+++ tests/Entity/Partner/PartnerTest.php
@@ -7,11 +7,31 @@
  */
 
 use Vespolina\Entity\Partner\Partner;
+use Vespolina\Entity\Partner\Address;
 
 class PartnerTest extends \PHPUnit_Framework_TestCase
 {
+    // todo: payment profile (cc profile)
     public function testPaymentProfileMethods()
     {
         $this->markTestIncomplete('tests for payment profiles (including cc) need to be written and implemented');
+    }
+
+    public function testFluentInterface()
+    {
+        $addressOnFile = new Address();
+
+        $partner = new Partner();
+        $partner
+            ->setShortName('jake')
+            ->setRoles(array('ROLE_USER'))
+            ->setName('customer')
+            ->setLanguage('English')
+            ->setCurrency('USD')
+            ->setType(Partner::INDIVIDUAL)
+            ->setAddresses(array($addressOnFile))
+        ;
+
+        $this->assertContains('ROLE_USER', $partner->getRoles());
     }
 }
