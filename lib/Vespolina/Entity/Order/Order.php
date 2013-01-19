b523d88f1e86ff0752c1227a0f54ee2b4c927372
Luis Cordova <cordoval@gmail.com>
2013-01-18 21:56:30 -0500

    put body of cart into order entity

--- lib/Vespolina/Entity/Order/Order.php
+++ lib/Vespolina/Entity/Order/Order.php
@@ -12,5 +12,74 @@ use Vespolina\Entity\Order\BaseOrder;
 
 class Order extends BaseOrder implements OrderInterface
 {
+    const STATE_OPEN = 'open';          //Available for change
+    const STATE_LOCKED = 'locked';      //Locked for processing
+    const STATE_CLOSED = 'closed';      //Closed after processing
+    const STATE_EXPIRED = 'expired';    //Unprocessed and expired
 
+    protected $followUp;
+    protected $paymentInstruction;
+
+    /**
+     * @inheritdoc
+     */
+    public function getExpiresAt()
+    {
+        return $this->expiresAt;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setExpiresAt(\DateTime $expiresAt)
+    {
+        $this->expiresAt = $expiresAt;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getFollowUp()
+    {
+        return $this->followUp;
+    }
+
+    public function setPaymentInstruction($paymentInstruction)
+    {
+        $this->paymentInstruction = $paymentInstruction;
+    }
+
+    public function getPaymentInstruction()
+    {
+        return $this->paymentInstruction;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setFollowUp($followUp)
+    {
+        $this->followUp = $followUp;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setPrice($name, $price)
+    {
+        $this->prices[$name] = $price;
+    }
+
+    public function autoSetCreatedAt()
+    {
+        if (null === $this->createdAt) {
+            $this->createdAt = new \DateTime();
+        }
+        $this->autoSetUpdatedAt();
+    }
+
+    public function autoSetUpdatedAt()
+    {
+        $this->updatedAt = new \DateTime();
+    }
 }
