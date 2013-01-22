5bb9f26a55d8ec898ffdedd8b61f63f57b415b1b
iampersistent <develop@zestic.com>
2013-01-22 01:00:25 -0800

    update the tests to the current needs

--- tests/Entity/PricingSetTest.php
+++ tests/Entity/PricingSetTest.php
@@ -1,7 +1,7 @@
 <?php
 
-use Vespolina\Entity\Pricing\PricingElement;
-use Vespolina\Entity\Pricing\PricingSet;
+use Pricing\Entity\PricingElement;
+use Pricing\Entity\PricingSet;
 
 class PricingSetTest extends \PHPUnit_Framework_TestCase
 {
@@ -28,18 +28,11 @@ class PricingSetTest extends \PHPUnit_Framework_TestCase
         $elementNetValue->expects($this->any())
             ->method('process')
             ->will($this->returnValue(array('netValue' => '9.99')));
-        $elementDiscount = $this->getMock('Vespolina\Entity\Pricing\PricingElement', array('process'));
-        $elementDiscount->expects($this->any())
-            ->method('process')
-            ->will($this->returnValue(array('discount' => '.99')));
 
         $pricingSet->process();
 
         $this->assertEqual('9.99', $pricingSet->getNetValue(), 'the final value should be 9.99');
-        $this->assertEqual('.99', $pricingSet->getDiscount(), 'the final value should be .99');
-        $this->assertEqual('9.00', $pricingSet->getValue(), 'the final value should be 9.00');
+        $this->assertEqual('9.99', $pricingSet->getValue(), 'the final value should be 9.00');
 
-        $this->markTestIncomplete('test all default returns');
-        $this->markTestIncomplete('test custom returns');
     }
 }
