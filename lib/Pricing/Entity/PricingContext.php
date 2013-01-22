1fbe120b879251788fb12e99d6966b2cbba96f3b
Luis Cordova <cordoval@gmail.com>
2013-01-22 07:29:24 -0500

    prepare and fix autoload

--- lib/Pricing/Entity/PricingContext.php
+++ lib/Pricing/Entity/PricingContext.php
@@ -1,12 +1,6 @@
 <?php
-/**
- * (c) 2011-2012 Vespolina Project http://www.vespolina-project.org
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
 
-namespace Pricing\Entity;
+namespace ImmersiveLabs\Pricing\Entity;
 
 /**
  * PricingContext implements a data container holding price variables needed for calculation
