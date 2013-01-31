e0e18e52f737bbcde57325b7de0e725f29539544
iampersistent <develop@zestic.com>
2013-01-31 00:19:09 -0800

    refactor to use Pimple based PricingContext

--- lib/Vespolina/Entity/Pricing/PricingContextInterface.php
+++ lib/Vespolina/Entity/Pricing/PricingContextInterface.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * (c) 2011-2012 Vespolina Project http://www.vespolina-project.org
+ * (c) 2011-2013 Vespolina Project http://www.vespolina-project.org
  *
  * (c) Daniel Kucharski <daniel@xerias.be>
  * This source file is subject to the MIT license that is bundled
@@ -11,39 +11,5 @@ namespace Vespolina\Entity\Pricing;
 
 interface PricingContextInterface
 {
-    /**
-     * Get the quantity of items for this pricing context. This is a shortcut for get('quantity')
-     *
-     * @return integer
-     */
-    function getQuantity();
 
-    /**
-     * Get the quantity of items for this pricing context
-     *
-     * @param integer
-     *
-     * @return $this
-     */
-    function setQuantity($quantity);
-
-    /**
-     * Get the value for a set variable in this context or null if the variable is not set
-     *
-     * @param $key
-     * @param null $default // todo, how would this work since the context is used for processing the PricingSet?
-     *
-     * @return null | mixed
-     */
-    function get($key, $default = null);
-
-    /**
-     * Set the value for a variable that will be used by a PricingSet
-     *
-     * @param string $key
-     * @param $value
-     *
-     * @return $this
-     */
-    function set($key, $value);
 }
--- tests/Entity/Pricing/PricingContextTest.php
+++ tests/Entity/Pricing/PricingContextTest.php
@@ -7,19 +7,6 @@ use Vespolina\Entity\Pricing\PricingContext;
  */
 class PricingContextTest extends \PHPUnit_Framework_TestCase
 {
-    public function testConstruct()
-    {
-        $data = array(
-            'something' => 'nothing',
-            'quantity' => 1
-        );
-        $context = new PricingContext($data);
-
-        $rp = new \ReflectionProperty($context, 'data');
-        $rp->setAccessible(true);
-        $this->assertSame($data, $rp->getValue($context), "the passed data should be put in the object's data property");
-    }
-
     public function testGet()
     {
         $data = array(
@@ -27,32 +14,6 @@ class PricingContextTest extends \PHPUnit_Framework_TestCase
         );
         $context = new PricingContext($data);
 
-        $this->assertSame('nothing', $context->get('something'), 'simple return of data');
-        $this->assertNull($context->get('bullshit'), 'there is no bullshit so a null should be returned');
-        $this->assertSame('Charlie Sheen', $context->get('winning', 'Charlie Sheen'), "the default value should be returned since there isn't a data set");
-        $this->assertSame('nothing', $context->get('something', 'something'), 'the default should only happen when there is no data'); // todo: this may need to change
-    }
-
-    public function testSetQuantity()
-    {
-        $context = new PricingContext();
-
-        $context->setQuantity(3);
-
-        $this->assertSame(3, $context->get('quantity'), "since this is a shortcut for set('quantity', 3) the get('quantity') should return the correct value");
-        $this->assertSame(3, $context->getQuantity(), 'the quantity that was set should be returned');
-    }
-
-    public function testGetQuantity()
-    {
-        $context = new PricingContext();
-
-        $this->assertSame(1, $context->getQuantity(), 'when the quantity has not been set, it should default to 1');
-        $this->assertSame(1, $context->get('quantity'), 'when the quantity has not been set, it should default to 1');
-
-        $context->setQuantity(4);
-        $this->assertSame(4, $context->getQuantity(), 'the quantity that was set by the setQuantity() method should be returned');
-        $context->set('quantity', 5);
-        $this->assertSame(5, $context->getQuantity(), 'the quantity that was set by the set() method should be returned');
+        $this->assertSame('nothing', $context['something'], 'simple return of data');
     }
 }
