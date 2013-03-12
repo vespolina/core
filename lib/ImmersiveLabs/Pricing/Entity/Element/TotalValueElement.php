5b309d8df37b4a54ea442b5499940a65c6d57d92
iampersistent <develop@zestic.com>
2013-03-11 17:24:27 -0700

    updates to pricing set code

--- lib/ImmersiveLabs/Pricing/Entity/Element/TotalValueElement.php
+++ lib/ImmersiveLabs/Pricing/Entity/Element/TotalValueElement.php
@@ -2,11 +2,26 @@
 
 namespace ImmersiveLabs\Pricing\Entity\Element;
 
+<<<<<<< HEAD:lib/ImmersiveLabs/Pricing/Entity/Element/TotalValueElement.php
 use ImmersiveLabs\Pricing\Entity\PricingElement;
+=======
+use Vespolina\Entity\Pricing\PricingElement;
+use Vespolina\Entity\Pricing\PricingElementValueInterface;
+>>>>>>> updates to pricing set code:lib/Vespolina/Entity/Pricing/Element/TotalValueElement.php
 
-class TotalValueElement extends PricingElement
+class TotalValueElement extends PricingElement implements PricingElementValueInterface
 {
     protected $position = 100000;
+
+    public function add($augend, $addend)
+    {
+        throw new FunctionNotSupportedException('add() has not been implemented in ' . get_class($this));
+    }
+
+    public function subtract($minuend, $subtrahend)
+    {
+        throw new FunctionNotSupportedException('subtract() has not been implemented in ' . get_class($this));
+    }
 
     protected function doProcess($context, $processed)
     {
