a7cb60465dd09f42ba7679c8530bfadb90647af5
iampersistent <develop@zestic.com>
2013-03-15 19:02:41 -0700

    CS for Partner add tests and methods for payment profiles

--- lib/Vespolina/Entity/Partner/PartnerInterface.php
+++ lib/Vespolina/Entity/Partner/PartnerInterface.php
@@ -9,6 +9,7 @@
 namespace Vespolina\Entity\Partner;
 
 use Vespolina\Entity\Partner\ContactInterface;
+
 /**
  * Partner interface, define basic fields
  * 
@@ -16,127 +17,6 @@ use Vespolina\Entity\Partner\ContactInterface;
  */
 interface PartnerInterface
 {
-    /**
-     * Sets the partnerId
-     * 
-     * @param string $partnerId
-     * @return \Vespolina\Entity\Partner\PartnerInterface
-     */
-    function setPartnerId($partnerId);
-    
-    /**
-     * Functional partner id
-     * 
-     * @return string
-     */
-    function getPartnerId();
-    
-    /**
-     * Date since when this partner joined
-     * @param \DateTime $partnerSince
-     * @return \Vespolina\Entity\Partner\PartnerInterface
-     */
-    function setPartnerSince(\DateTime $partnerSince);
-    
-    /**
-     * @return DateTime
-     */
-    function getPartnerSince();
-
-    /**
-     * THe role of the Partner (e.g. Customer, Supplier, Employee)
-     * @return string
-     */
-    function getRoles();
-    
-    /**
-     * Adds a role to the current Partner
-     * 
-     * @param string $role
-     * @return \Vespolina\Entity\Partner\PartnerInterface
-     */
-    function addRole($role);
-    
-    /**
-     * Sets the roles of the partner
-     * 
-     * @param array $roles
-     * @return \Vespolina\Entity\Partner\PartnerInterface
-     */
-    function setRoles($roles);
-
-    /**
-     * Sets the name of the partner
-     * 
-     * @param string $name
-     * @return \Vespolina\Entity\Partner\PartnerInterface
-     */
-    function setName($name);
-    
-    /**
-     * Name for partner
-     * 
-     * @return string
-     */
-    function getName();
-
-    /**
-     * Sets the partners default currency (ISO-4217)
-     * @param string $currency
-     * @return \Vespolina\Entity\Partner\PartnerInterface
-     */
-    function setCurrency($currency);
-    
-    /**
-     * Default currency for partner
-     * 
-     * @return string - ISO-4217
-     */
-    function getCurrency();
-    
-    /**
-     * Sets the partners default language
-     * 
-     * @param string $language - IETF tag
-     * @return \Vespolina\Entity\Partner\PartnerInterface
-     */
-    function setLanguage($language);
-    
-    /**
-     * Default language for partner
-     * 
-     * @return string - IETF tag
-     */
-    function getLanguage();
-    
-    /**
-     * Sets the default payment terms for customer
-     * 
-     * @param string $paymentTerms
-     * @return \Vespolina\Entity\Partner\PartnerInterface
-     */
-    function setPaymentTerms($paymentTerms);
-    
-    /**
-     * Default payment terms for partner
-     */
-    function getPaymentTerms();
-    
-    /**
-     * Sets the partners type
-     * 
-     * @param string $type
-     * @return \Vespolina\Entity\Partner\PartnerInterface
-     */
-    function setType($type);
-    
-    /**
-     * Discriminator field
-     * 
-     * @return string
-     */
-    function getType();
-    
     /**
      * Sets the partners addresses
      * 
@@ -151,7 +31,7 @@ interface PartnerInterface
      * @param \Vespolina\Entity\Partner\Address $address
      * @return \Vespolina\Entity\Partner\PartnerInterface
      */
-    function addAddress($address);
+    function addAddress(AddressInterface $address);
     
     /**
      * Addresses for this account
@@ -159,7 +39,143 @@ interface PartnerInterface
      * @return \Vespolina\Entity\Partner\Address[]
      */
     function getAddresses();
-    
+
+    /**
+     * Sets the partners default currency (ISO-4217)
+     * @param string $currency
+     * @return \Vespolina\Entity\Partner\PartnerInterface
+     */
+    function setCurrency($currency);
+
+    /**
+     * Default currency for partner
+     *
+     * @return string - ISO-4217
+     */
+    function getCurrency();
+
+    /**
+     * Sets the partners default language
+     *
+     * @param string $language - IETF tag
+     * @return \Vespolina\Entity\Partner\PartnerInterface
+     */
+    function setLanguage($language);
+
+    /**
+     * Default language for partner
+     *
+     * @return string - IETF tag
+     */
+    function getLanguage();
+
+    /**
+     * Sets the name of the partner
+     *
+     * @param string $name
+     * @return \Vespolina\Entity\Partner\PartnerInterface
+     */
+    function setName($name);
+
+    /**
+     * Name for partner
+     *
+     * @return string
+     */
+    function getName();
+
+    /**
+     * Sets the organisation details
+     *
+     * @param $organisationDetails
+     * @return \Vespolina\Entity\Partner\PartnerInterface
+     */
+    function setOrganisationDetails(OrganisationDetailsInterface $organisationDetails);
+
+    /**
+     * The organisation details for this partner (used for type organisation)
+     *
+     * @return \Vespolina\Entity\Partner\OrganisationDetailsInterface
+     */
+    function getOrganisationDetails();
+
+    /**
+     * Sets the partnerId
+     *
+     * @param string $partnerId
+     * @return \Vespolina\Entity\Partner\PartnerInterface
+     */
+    function setPartnerId($partnerId);
+
+    /**
+     * Functional partner id
+     *
+     * @return string
+     */
+    function getPartnerId();
+
+    /**
+     * Date since when this partner joined
+     * @param \DateTime $partnerSince
+     * @return \Vespolina\Entity\Partner\PartnerInterface
+     */
+    function setPartnerSince(\DateTime $partnerSince);
+
+    /**
+     * @return \DateTime
+     */
+    function getPartnerSince();
+
+    /**
+     * Sets the default payment terms for customer
+     *
+     * @param string $paymentTerms
+     * @return \Vespolina\Entity\Partner\PartnerInterface
+     */
+    function setPaymentTerms($paymentTerms);
+
+    /**
+     * Default payment terms for partner
+     */
+    function getPaymentTerms();
+
+    /**
+     * Sets the personal details for this partner
+     *
+     * @param $personalDetails
+     * @return \Vespolina\Entity\Partner\PartnerInterface
+     */
+    function setPersonalDetails($personalDetails);
+
+    /**
+     * Personal details of partner (used for type individual)
+     *
+     * @return \Vespolina\Entity\Partner\PersonalDetailsInterface
+     */
+    function getPersonalDetails();
+
+    /**
+     * Clear all of the payment profiles
+     *
+     * @return $this
+     */
+    function clearPaymentProfiles();
+
+    /**
+     * Set the preferred payment profile
+     *
+     * @param \Vespolina\Entity\Partner\PaymentProfileInterface $paymentProfile
+     * @return $this
+     */
+    function setPreferredPaymentProfile(PaymentProfileInterface $paymentProfile);
+
+    /**
+     * Return the preferred payment profile
+     *
+     * @return \Vespolina\Entity\Partner\PaymentProfileInterface
+     */
+    function getPreferredPaymentProfile();
+
     /**
      * Sets the partners primary contact
      * 
@@ -174,34 +190,41 @@ interface PartnerInterface
      * @return \Vespolina\Entity\Partner\Contact
      */
     function getPrimaryContact();
-    
+
     /**
-     * Sets the personal details for this partner
-     * 
-     * @param $personalDetails
+     * THe role of the Partner (e.g. Customer, Supplier, Employee)
+     * @return string
+     */
+    function getRoles();
+
+    /**
+     * Adds a role to the current Partner
+     *
+     * @param string $role
      * @return \Vespolina\Entity\Partner\PartnerInterface
      */
-    function setPersonalDetails($personalDetails);
-    
+    function addRole($role);
+
     /**
-     * Personal details of partner (used for type individual)
-     * 
-     * @return \Vespolina\Entity\Partner\PersonalDetailsInterface
-     */
-    function getPersonalDetails();
-    
-    /**
-     * Sets the organistaion details
-     * 
-     * @param $organisationDetails
+     * Sets the roles of the partner
+     *
+     * @param array $roles
      * @return \Vespolina\Entity\Partner\PartnerInterface
      */
-    function setOrganisationDetails($organisationDetails);
-    
+    function setRoles($roles);
+
     /**
-     * The organistaion details for this partner (used for type organisation)
-     * 
-     * @return \Vespolina\Entity\Partner\OrganisationDetailsInterface
+     * Sets the partners type
+     *
+     * @param string $type
+     * @return \Vespolina\Entity\Partner\PartnerInterface
      */
-    function getOrganisationDetails();
+    function setType($type);
+
+    /**
+     * Discriminator field
+     *
+     * @return string
+     */
+    function getType();
 }
