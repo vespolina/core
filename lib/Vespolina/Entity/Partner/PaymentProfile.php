4d7000d17768d61df72b75499790f4b56511882c
iampersistent <develop@zestic.com>
2013-03-15 19:02:02 -0700

    updates to payment profiles, add bitcoin profile

--- lib/Vespolina/Entity/Partner/PaymentProfile.php
+++ lib/Vespolina/Entity/Partner/PaymentProfile.php
@@ -6,23 +6,17 @@ use Vespolina\Entity\Partner\PartnerInterface;
 
 class PaymentProfile implements PaymentProfileInterface
 {
-    const PAYMENT_PROFILE_TYPE_CREDIT_CARD = 'Credit Card';
-    const PAYMENT_PROFILE_TYPE_INVOICE = 'Invoice';
-
-    public static $validTypes = array(
-        self::PAYMENT_PROFILE_TYPE_CREDIT_CARD,
-        self::PAYMENT_PROFILE_TYPE_INVOICE,
-    );
-
     protected $id;
     protected $reference;
     protected $partner;
-    protected $billingCity;
-    protected $billingCountry;
-    protected $billingState;
-    protected $billingAddress;
-    protected $billingZipCode;
-    protected $billingPhone;
+
+    /**
+     * @inheritdoc
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
     /**
      * @inheritdoc
@@ -45,14 +39,6 @@ class PaymentProfile implements PaymentProfileInterface
     /**
      * @inheritdoc
      */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @inheritdoc
-     */
     public function setReference($reference)
     {
         $this->reference = $reference;
@@ -66,65 +52,5 @@ class PaymentProfile implements PaymentProfileInterface
     public function getReference()
     {
         return $this->reference;
-    }
-
-    public function setBillingAddress($billingAddress)
-    {
-        $this->billingAddress = $billingAddress;
-    }
-
-    public function getBillingAddress()
-    {
-        return $this->billingAddress;
-    }
-
-    public function setBillingZipCode($billingZipCode)
-    {
-        $this->billingZipCode = $billingZipCode;
-    }
-
-    public function getBillingZipCode()
-    {
-        return $this->billingZipCode;
-    }
-
-    public function setBillingCity($billingCity)
-    {
-        $this->billingCity = $billingCity;
-    }
-
-    public function getBillingCity()
-    {
-        return $this->billingCity;
-    }
-
-    public function setBillingCountry($billingCountry)
-    {
-        $this->billingCountry = $billingCountry;
-    }
-
-    public function getBillingCountry()
-    {
-        return $this->billingCountry;
-    }
-
-    public function setBillingPhone($billingPhone)
-    {
-        $this->billingPhone = $billingPhone;
-    }
-
-    public function getBillingPhone()
-    {
-        return $this->billingPhone;
-    }
-
-    public function setBillingState($billingState)
-    {
-        $this->billingState = $billingState;
-    }
-
-    public function getBillingState()
-    {
-        return $this->billingState;
     }
 }
