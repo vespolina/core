5081afb5b6b80b0ba41444f910e96cc519837479
iampersistent <develop@zestic.com>
2013-03-17 03:02:18 -0700

    add PaymentRequest, BillingRequest inherits from it

---
+++ lib/Vespolina/Entity/Payment/PaymentRequestInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * (c) 2013 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Vespolina\Entity\Payment;
+
+interface PaymentRequestInterface 
+{
+
+}
