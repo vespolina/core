5d3030f39fe5c0cabb411b8979b611b7a2200a07
iampersistent <develop@zestic.com>
2013-03-19 03:52:38 -0700

    add PaymentContext

---
+++ lib/Vespolina/Entity/Payment/PaymentContext.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * (c) 2013 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Vespolina\Entity\Payment;
+
+/**
+ * PaymentContext implements a data container holding payment variables needed for payment handling
+ *
+ */
+class PaymentContext extends \Pimple
+{
+    /**
+     * Convenience getter
+     *
+     * @param $key
+     * @return mixed
+     */
+    public function get($key, $default = null)
+    {
+        if ($this->offsetExists($key)) {
+            return $this->offsetGet($key);
+        }
+
+        return $default;
+    }
+
+    /**
+     * Convenience setter
+     *
+     * @param $key
+     * @param $value
+     */
+    public function set($key, $value)
+    {
+        return $this->offsetSet($key, $value);
+    }
+}
