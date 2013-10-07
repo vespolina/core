63161580ee796feb45d2c899fb8a8ab71d78ce63
Richard Shank <develop@zestic.com>
2013-10-07 02:30:33 -0700

    add DirectDeposit payment profile

---
+++ lib/Vespolina/Entity/Payment/PaymentProfile/DirectDeposit.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * (c) 2011 - ∞ Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Vespolina\Entity\Payment\PaymentProfile;
+
+use Vespolina\Entity\Payment\PaymentProfile;
+
+class DirectDeposit extends PaymentProfile
+{
+    public function getType()
+    {
+        return 'Direct Deposit';
+    }
+}
